f6a33cac3dec531acad230ae61d931cfe6884070
Eric Valasek <cire333@gmail.com>
2025-10-17 15:08:23 -0700

    last session free login

--- frontend/bedroqui/src/index.tsx
+++ frontend/bedroqui/src/index.tsx
@@ -17,3 +17,27 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+
+// Once Auth is added, wrap <App /> in <AuthProvider> as shown below:
+// import { AuthProvider } from "react-oidc-context";
+
+// const cognitoAuthConfig = {
+//   authority: "https://cognito-idp.us-east-2.amazonaws.com/us-east-2_kgIH6ZIif",
+//   client_id: "3ahaoavqt9lf07j2btnv26d5dr",
+//   redirect_uri: "https://d84l1y8p4kdic.cloudfront.net",
+//   response_type: "code",
+//   scope: "phone openid email",
+// };
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// // wrap the application with AuthProvider
+// root.render(
+//   <React.StrictMode>
+//     <AuthProvider {...cognitoAuthConfig}>
+//       <App />
+//     </AuthProvider>
+//   </React.StrictMode>
+// );
